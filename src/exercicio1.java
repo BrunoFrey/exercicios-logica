61b98813e5ccfc2069d687a99eec4ce220754f7a
bruno.frey <freym3368@gmail.com>
2025-07-15 15:17:43 -0300

    Create project

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -1,8 +1,21 @@
 import java.sql.SQLOutput;
 
+import java.util.Scanner; //usada para ler as entradas do usuario //biblioteca
+
 public class exercicio1 {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Scanner entrada = new Scanner(System.in); // cria o scanner, para ler oque o usuario digita no teclado
 
+        System.out.print("Digite sua idade: ");
+        int idade = entrada.nextInt(); // le um número inteiro digitado
+
+        if (idade >= 18) {
+            System.out.println("A pessoa é maior de idade.");
+        } else {
+            System.out.println("A pessoa é menor de idade.");
+        }
+
+        entrada.close(); // fecha o scanner
     }
 }
+
