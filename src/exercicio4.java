2217391f0f2f7f3e28b8e482cd940b2984d74a98
bruno.frey <freym3368@gmail.com>
2025-07-25 13:43:30 -0300

    Exercicio 4

---
+++ src/exercicio4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Escolha a operação:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+
+        int opcao = sc.nextInt();
+
+        //pede os numeros
+        System.out.println("Digite o primeiro número:");
+        int num1 = sc.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int num2 = sc.nextInt();
+        // estrutura de decição
+        // switch-case , serve para escolher uma opção entre várias
+        switch (opcao) {
+            case 1:
+                System.out.println("Resultado da soma: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Resultado da subtração: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Resultado da multiplicação: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) { // nao pode dividr por zero
+                    System.out.println("Resultado da divisão: " + (num1 / num2));
+                } else {
+                    System.out.println("Erro: Não pode dividir por zero.");
+                }
+                break;
+            default: // para o programa responder mesmo se o usuario por um numero desconhecido
+                System.out.println("Opção inválida.");
+        }
+
+        sc.close();
+    }
+}
