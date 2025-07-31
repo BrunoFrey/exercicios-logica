9808fdc5f16afd743704738376c4c4e75a90fa2a
bruno.frey <freym3368@gmail.com>
2025-07-31 14:09:34 -0300

    Exercicio 3

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class exercicio3 {
+public class Exercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
