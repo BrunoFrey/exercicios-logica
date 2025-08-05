a73f17f05aa712ac68a4bb90ed6718eb011ba3ca
bruno.frey <freym3368@gmail.com>
2025-08-05 13:31:32 -0300

    Exercicio 5

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -14,3 +14,8 @@ public class Exercicio5 {
         sc.close();
     }
 }
+
+// for é responsável por repetir
+// declara a variavel i como int e coloca i <= 10; pra for fazer repetir de 1 a 10
+// i++ , significa que ele começa como 1 e vai repetindo 1,2,3,4,5... ate o 10
+// quando o i++ chega no 11 ele para, por conta do i <= 10;
