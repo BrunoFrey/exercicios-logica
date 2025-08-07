98164bff4ebfc040a03812368c4c99bb2aa93fe9
bruno.frey <freym3368@gmail.com>
2025-08-07 10:20:45 -0300

    Exercicio 8

--- src/Exercício8.java
+++ src/Exercício8.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Exercício8 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int numero = 0;
 
         while (numero != 10) {
             System.out.print("Digite um número:");
-            numero = sc.nextInt();
+            numero = scanner.nextInt();
         }
 
         System.out.println("Você digitou 10. Programa encerrado.");
-        sc.close();
+        scanner.close();
     }
 }
