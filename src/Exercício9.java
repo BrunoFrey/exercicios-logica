0260d745977e932e082a21d016ddf0a53a6d9c3c
bruno.frey <freym3368@gmail.com>
2025-08-07 10:20:51 -0300

    Exercicio 9

---
+++ src/Exercício9.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercício9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número limite para a sequência: ");
+        int limite = scanner.nextInt();
+
+        int primeiro = 0;
+        int segundo = 1;
+
+        System.out.println("Série de Fibonacci até " + limite + ":");
+
+        while (primeiro <= limite) {
+            System.out.print(primeiro + " ");
+
+            int proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+        }
+
+        scanner.close();
+    }
+}
