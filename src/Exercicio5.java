f203507d2553db62e36a5adb5e294f6b2a5f035e
bruno.frey <freym3368@gmail.com>
2025-08-07 10:20:17 -0300

    Exercicio 5

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class Exercicio5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um número:");
-        int numero = sc.nextInt();
+        int numero = scanner.nextInt();
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(numero + " x " + i + " = " + (numero * i));
         }
 
-        sc.close();
+        scanner.close();
     }
 }
 
-// for é responsável por repetir
-// declara a variavel i como int e coloca i <= 10; pra for fazer repetir de 1 a 10
-// i++ , significa que ele começa como 1 e vai repetindo 1,2,3,4,5... ate o 10
-// quando o i++ chega no 11 ele para, por conta do i <= 10;
+
